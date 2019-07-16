e1be6f497baf944d5eec5111959e9b9d326f282d
paraline <fviktor1005@gmail.com>
2019-07-16 18:51:44 +0200

    add readme info

--- src/Modules/Firebase/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAeVrsLbO79LGFFUwF5s1a_eK16gqqLIKE",
-  authDomain: "chat-d099a.firebaseapp.com",
-  databaseURL: "https://chat-d099a.firebaseio.com",
-  projectId: "chat-d099a",
-  storageBucket: "chat-d099a.appspot.com",
-  messagingSenderId: "776792551775",
-  appId: "1:776792551775:web:10305f4b97c81b91"
-};
